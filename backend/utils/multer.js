2549b361846ce8f3b38f803341ff7acfd8ed660e
Hasnain Ali <hasnain5f7@gmail.com>
2026-01-04 21:46:03 +0500

    Add multer configuration for image uploads and validation middleware for signup and login

---
+++ backend/utils/multer.js
@@ -0,0 +1,25 @@
+import multer from 'multer'
+import path from 'path'
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads/')
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now();
+        cb(null, uniqueSuffix + path.extname(file.originalname))
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true)
+    }
+    else {
+        cb(new Error('Only image files are allowed!'), false)
+    }
+}
+
+const upload = multer({ storage, fileFilter, limits: { fileSize: 2 * 1024 * 1024 } }) 
+
+export default upload;
---
+++ backend/validation/validation.js
@@ -0,0 +1,38 @@
+import { body } from "express-validator";
+
+const signupValidation = [
+    body("fullName")
+        .notEmpty()
+        .withMessage("Full name is required")
+        .isAlpha()
+        .withMessage("Fullname can't contain numerics")
+        .isLength({ min: 3 })
+        .withMessage("Full name must be at least 3 characters long"),
+    body('email')
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Plz provide a valid Email'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6, max: 15 })
+        .withMessage('Password must 6 to 15 character long')
+];
+
+const loginValidation = [
+    body('email')
+        .notEmpty()
+        .withMessage('Email is required')
+        .isEmail()
+        .withMessage('Plz provide a valid Email'),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 6, max: 15 })
+        .withMessage('Password must 6 to 15 character long')
+];
+
+export { signupValidation, loginValidation };
